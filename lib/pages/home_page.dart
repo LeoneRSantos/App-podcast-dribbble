10075df1dfce3dce38e5ace4883750106a9b3b4f
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-07-19 23:33:57 -0300

    Refatoração na home

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    ColorScheme cores = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      backgroundColor: cores.background,
+  
+      body: Column(
+
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
