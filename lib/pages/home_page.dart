1398be7a4b06fd2f402ca5529d1947508e772e7f
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-07-20 00:01:16 -0300

    Alterações na home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:podcast_app_dribbble/widgets/textos.dart';
+import 'package:podcast_app_dribbble/widgets/topo.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -14,15 +16,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
     ColorScheme cores = Theme.of(context).colorScheme;
 
-    return Scaffold(
-      backgroundColor: cores.background,
-  
-      body: Column(
-
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          
-        ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: cores.background,
+      
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+    
+          children: [
+            Topo(),
+          ],
+        ),
       ),
     );
   }
