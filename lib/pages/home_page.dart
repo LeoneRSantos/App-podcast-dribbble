7bd5b70882f745143c5ef7b170290f4b1a409280
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-07-26 17:58:18 -0300

    Adição do sliding_clipped_nav_bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:podcast_app_dribbble/widgets/categorias_topo.dart';
 import 'package:podcast_app_dribbble/widgets/textos.dart';
 import 'package:podcast_app_dribbble/widgets/topo.dart';
+import 'package:sliding_clipped_nav_bar/sliding_clipped_nav_bar.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class _MyHomePageState extends State<MyHomePage> {
   
   double _tamanhoInicialTexto = 22.0;
   Color _corInicialTexto = Color(0xFFB5B6B6);
+  int selectedIndex = 0;
 
   void _categoriaSelecionada(){
     if (_corInicialTexto == Color(0xFFB5B6B6)) {
@@ -57,6 +59,37 @@ class _MyHomePageState extends State<MyHomePage> {
           selecionarCategoria: _categoriaSelecionada,
           tamanhoTexto: _tamanhoInicialTexto,),
         
+        ],
+      ),
+
+      bottomNavigationBar: SlidingClippedNavBar(
+        backgroundColor: Colors.white,
+        onButtonPressed: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        iconSize: 30,
+        activeColor: Color(0xFF01579B),
+        selectedIndex: selectedIndex,
+        barItems: [
+          BarItem(
+            icon: Icons.home,
+            title: 'Home',
+          ),
+          BarItem(
+            icon: Icons.search_rounded,
+            title: 'Explorar',
+          ),
+          BarItem(
+            icon: Icons.bookmark_outlined,
+            title: 'Salvos',
+          ),
+          BarItem(
+            icon: Icons.settings_outlined,
+            title: 'Configurações',
+          ),
+           /// Add more BarItem if you want
         ],
       ),
     ),
