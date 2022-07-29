cd82932e75492388dff81a4b2a376dbbebb9fa33
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-07-28 22:52:52 -0300

    Criação da classe CategoriaTopo

---
+++ lib/categoria_topo.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CategoriaTopo{
+  
+  static const Color corInativa = Color(0xFFB5B6B6);
+  static const Color corAtiva = Color(0xFF050507);
+
+  double tamanhoTextoAtivo = 26.0;
+  double tamanhoTextoInativo = 20.0;
+
+  int _indiceCategoria = 0;
+
+  int verificarIndiceCategoria(String nomeDaCategoria){
+    
+    if (nomeDaCategoria == 'Popular') {
+      _indiceCategoria = 0;
+      debugPrint('Indice da categoria: $_indiceCategoria');
+      return _indiceCategoria;
+    }
+    
+    else if(nomeDaCategoria == 'Destaque'){
+      _indiceCategoria = 1;
+      debugPrint('Indice da categoria: $_indiceCategoria');
+      return _indiceCategoria;
+    }
+
+    else if(nomeDaCategoria == 'Tendências'){
+      _indiceCategoria = 2;
+      debugPrint('Indice da categoria: $_indiceCategoria');
+      return _indiceCategoria;
+    }
+
+    debugPrint('Indice da categoria: $_indiceCategoria');
+    return 0;
+  }
+
+}
