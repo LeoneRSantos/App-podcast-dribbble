70894e52e5885c5d57bd98a6ca602f4d01960659
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-07-31 11:36:36 -0300

    Criação de ReproduzidosRecentemente

---
+++ lib/widgets/reproduzidos_recentemente.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class ReproduzidosRecentemente extends StatelessWidget {
+  ReproduzidosRecentemente({Key? key}) : super(key: key);
+  
+  // Logo dos Podcasts
+  String aDerivaImagem = 'assets/images/a-deriva.jpg';
+  String flowImagem = 'assets/images/flow.jpg';
+  String podPahImagem = 'assets/images/podpah.jpg';
+
+  // Nomes dos podcasts
+  String aDerivaNome = 'À Deriva podcast';
+  String flowNome = 'Flow podcast';
+  String podPahNome = 'Pod Pah podcast';
+  
+  // Lista de imagens
+  late List<String> imagensPodcasts = [ 
+    aDerivaImagem, flowImagem, podPahImagem, aDerivaImagem, flowImagem, podPahImagem,
+  ];
+
+  // Lista de Nomes (subtítulos)
+  late List<String> nomesPodcasts = [ 
+    aDerivaNome, flowNome, podPahNome, aDerivaNome, flowNome, podPahNome,
+  ];
+
+  // Lista de títulos
+  List<String> titulosPodcasts = [ 
+    'Caio Fábio (191) | À Deriva Podcast com Arthur Petry', 
+    'FALCÃO 12 - Flow #74 🤝 @Flow Sport Club', 
+    'MANO BROWN - Podpah #351',
+
+    'Taryana Rocha (Psicoterapeuta) (129) | À Deriva Podcast com Arthur Petry',
+    'BRUNO CORREA [+ RATO BORRACHUDO] - Flow #84',
+    'WHINDERSSON NUNES - Podpah #316',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: imagensPodcasts.length,
+      itemBuilder: (BuildContext context, int index) {
+        
+        return ComponentePlayer(
+          imagem: imagensPodcasts[index], 
+          titulo: titulosPodcasts[index], 
+          nome: nomesPodcasts[index],);
+
+      });
+  }
+}
+
+class ComponentePlayer extends StatelessWidget {
+  ComponentePlayer({Key? key, 
+  required this.imagem, 
+  required this.titulo, 
+  required this.nome}) : super(key: key);
+
+  final String imagem;
+  final String titulo;
+  final String nome;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row( 
+      children: [ 
+        CircleAvatar( 
+          radius: 20.0,
+          child: Image.asset(imagem),
+        ),
+
+        Expanded(
+          child: ListTile( 
+            style:ListTileStyle.list,
+            title: Text(titulo),
+            subtitle: Text(nome),
+          ),
+        ),
+
+        Spacer(), 
+
+        Container(
+          decoration: BoxDecoration( 
+              borderRadius: BorderRadius.circular(50.0),
+              border: Border.all( 
+                width: 1.0, 
+                color: Theme.of(context).colorScheme.primary,
+              )
+            ),
+          child: IconButton(onPressed: (){}, 
+          icon: Icon(Icons.play_arrow, 
+          color: Theme.of(context).colorScheme.onBackground,)),
+        ),
+      ],
+    );
+  }
+}
