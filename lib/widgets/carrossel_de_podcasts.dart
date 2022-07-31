d46892a6ea1a50c6ac2cd153b9dd571a3b47a5b8
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-07-31 00:46:27 -0300

    Criação do Widget CarrosselDePodcasts

---
+++ lib/widgets/carrossel_de_podcasts.dart
@@ -0,0 +1,38 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class CarroselDePodcasts extends StatelessWidget {
+  CarroselDePodcasts({Key? key}) : super(key: key);
+
+  List<String> imagens = ['assets/images/a-deriva.jpg', 'assets/images/flow.jpg', 'assets/images/podpah.jpg'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CarouselSlider.builder( 
+        
+      itemBuilder: (BuildContext context, int index, int realIndex) { 
+        final podcasts = imagens[index];
+
+        return construirImagem(podcasts, index);
+       }, 
+      itemCount: imagens.length, 
+      options: CarouselOptions( 
+        height: 300.0, 
+        autoPlay: true, 
+        reverse: true, 
+        enlargeCenterPage: true,
+        enableInfiniteScroll: true,
+        enlargeStrategy: CenterPageEnlargeStrategy.height,
+        pageSnapping: false,
+      ),),
+    );
+  }
+}
+
+Widget construirImagem(String imagem, int indice) => Container( 
+  margin: EdgeInsets.symmetric(horizontal: 10.0),
+  child: Image.asset(imagem),
+);
+
+
