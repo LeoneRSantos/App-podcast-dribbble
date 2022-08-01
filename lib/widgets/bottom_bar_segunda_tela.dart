ff294afe27ada4d4b479e6bab748ee939a7feac1
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-08-01 10:48:13 -0300

    Criação da BottomBar da segunda tela

---
+++ lib/widgets/bottom_bar_segunda_tela.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class BottomBarSegundaTela extends StatefulWidget {
+  BottomBarSegundaTela({Key? key, 
+  required this.iconeSalvar, 
+  required this.iconePlay, 
+  required this.play, 
+  required this.salvar}) : super(key: key);
+
+  final IconData iconeSalvar;
+  final IconData iconePlay;
+  final void Function() play;
+  final void Function() salvar;
+
+  @override
+  State<BottomBarSegundaTela> createState() => _BottomBarSegundaTelaState();
+}
+
+class _BottomBarSegundaTelaState extends State<BottomBarSegundaTela> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row( 
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+        children: [ 
+          GestureDetector( 
+            onTap: widget.salvar,
+            child: ComponenteIcone(icone: widget.iconeSalvar)),
+
+          ComponenteIcone(icone: Icons.skip_previous),
+
+          Container(
+            decoration: BoxDecoration( 
+              borderRadius: BorderRadius.circular(50.0),
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            child: IconButton(
+              color: Theme.of(context).colorScheme.background,
+              onPressed: widget.play, 
+              icon: Icon(widget.iconePlay)),
+          ),
+
+          ComponenteIcone(icone: Icons.skip_next), 
+
+          ComponenteIcone(icone: Icons.arrow_downward_rounded),
+        ],
+      ),
+    );
+  }
+}
+
+class ComponenteIcone extends StatelessWidget {
+  ComponenteIcone({Key? key, 
+  required this.icone}) : super(key: key);
+
+  final IconData icone;
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(icone, size: 25.0, 
+        color: Theme.of(context).colorScheme.primary,);
+  }
+}
