ac011c6a9e91bb7f4afee71737d11c259e2a5ab4
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-08-08 16:45:52 -0300

    Estrutura inicial da tela Explorar

---
+++ lib/pages/explorar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Explorar extends StatefulWidget {
+  Explorar({Key? key}) : super(key: key);
+
+  @override
+  State<Explorar> createState() => _ExplorarState();
+}
+
+class _ExplorarState extends State<Explorar> {
+
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextField(
+          controller: _controller,
+          onSubmitted: (String value) async {
+            await showDialog<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Thanks!'),
+                  content: Text(
+                      'You typed "$value", which has length ${value.characters.length}.'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
