c7aa38f5e904a4a781bc37d1408d324d1fe6c245
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-08-09 17:20:11 -0300

    Refatoração no Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:podcast_app_dribbble/pages/pagina_inicial.dart';
+import 'my_app.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'OC Pajaro Medium',
-        colorScheme: const ColorScheme( 
-          brightness: Brightness.light, 
-          primary: Color(0xFF050507), 
-          secondary: Color(0xFFB5B6B6), 
-          onPrimary: Color(0xFFEB672B), 
-          onSecondary: Color(0xFF424153), 
-          background: Color(0XFFfbfdff), 
-          onBackground: Color(0xFFF1A060), 
-          surface: Color(0xFFB5B6B6), 
-          onSurface: Colors.amber, 
-          error: Colors.red, 
-          onError: Colors.red, 
-          tertiary: Color(0xFFDBB3AD)),
-        
-      ),
-      home: PaginaInicial(),
-    );
-  }
-}
 
 
